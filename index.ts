4a3c7fac9a835b651b4c9e6d773b778c12e888c7
Rohan Singla <rohansinglawork@gmail.com>
2026-05-30 19:41:35 +0530

    redis banner api

--- index.ts
+++ index.ts
@@ -6,6 +6,8 @@ const app = express();
 
 const redis =  new Redis(process.env.REDIS_URL || 'redis://localhost:6379');
 
+const banner_key = "app:banner";
+
 app.get("/redis", async (req,res) =>{
 
     const result = await redis.ping();
@@ -29,6 +31,51 @@ app.get('/mongo' , async (req,res) => {
     })
 
 })
+
+// site banner
+
+app.post("/banner" , async (req,res) => {
+    await redis.set(banner_key,req.body.message);
+
+
+    res.send({
+        success : true
+    })
+})
+
+app.get("/banner" , async (req,res) => {
+
+    const result = await redis.get(banner_key);
+
+
+    res.send({
+        message : result
+    })
+})
+
+
+app.delete("/banner" , async (req,res) => {
+
+    const result = await redis.del(banner_key);
+
+
+    res.send({
+        sucess : true
+    })
+})
+
+
+app.get("/banner/exists" , async (req,res) => {
+
+    const result = await redis.exists(banner_key);
+
+
+    res.send({
+        exists : Boolean(result)
+    })
+})
+
+
 
 app.listen(3000, ()=>{
     console.log("server running on port 3000!")
